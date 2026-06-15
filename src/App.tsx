3c05949261004c36ed6bd87d73501aa565552430
rontubot <rontubot@gmail.com>
2026-06-14 22:21:09 -0300

    act

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1000);
+    }, 2000);
     return () => clearTimeout(timer);
   }, []);
 
